68bdbf598b2f0271df1301869fc6940333802d66
Tim Harper <timcharper@gmail.com>
2008-11-23 00:34:02 -0700

    Rails 2.2 compatible

--- lib/calendar_date_select.rb
+++ lib/calendar_date_select.rb
@@ -6,6 +6,12 @@ if Object.const_defined?(:Rails) && File.directory?(Rails.root + "/public")
   ActionView::Base.send(:include, CalendarDateSelect::FormHelper)
   ActionView::Base.send(:include, CalendarDateSelect::IncludesHelper)
 
+  if ([Rails::VERSION::MAJOR, Rails::VERSION::MINOR] <=> [2, 2]) == -1
+    puts "This version of calendar date select (#{CalendarDateSelect.version}) requires Rails 2.2"
+    puts "To use an earlier version of rails, use calendar_date_select version 1.11.x"
+    exit
+  end
+    
   # install files
   unless File.exists?(RAILS_ROOT + '/public/javascripts/calendar_date_select/calendar_date_select.js')
     ['/public', '/public/javascripts/calendar_date_select', '/public/stylesheets/calendar_date_select', '/public/images/calendar_date_select', '/public/javascripts/calendar_date_select/locale'].each do |dir|
@@ -15,5 +21,4 @@ if Object.const_defined?(:Rails) && File.directory?(Rails.root + "/public")
       FileUtils.cp(Dir.glob(source+'/*.*'), dest)
     end
   end
-
 end
--- lib/calendar_date_select/calendar_date_select.rb
+++ lib/calendar_date_select/calendar_date_select.rb
@@ -1,5 +1,5 @@
 class CalendarDateSelect
-  VERSION = '1.11.1'
+  VERSION = '1.12'
   FORMATS = {
     :natural => {
       :date => "%B %d, %Y",
@@ -176,7 +176,7 @@ class CalendarDateSelect
           nil
         end
 
-      tag = ActionView::Helpers::InstanceTag.new(object, method, self, nil, options.delete(:object))
+      tag = ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object))
       calendar_date_select_output(
         tag.to_input_field_tag( (calendar_options[:hidden] || calendar_options[:embedded]) ? "hidden" : "text", options), 
         calendar_options
