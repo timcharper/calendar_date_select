f87d3efde648118eb0c687493d155c5d429ba023
Tim Harper <timcharper@gmail.com>
2009-01-08 12:33:09 -0700

    :format is not a valid option for calendar_date_select_javascripts

--- lib/calendar_date_select/includes_helper.rb
+++ lib/calendar_date_select/includes_helper.rb
@@ -7,7 +7,7 @@ module CalendarDateSelect::IncludesHelper
 
   # returns an array of javascripts needed for the selected locale, date_format, and calendar control itself.
   def calendar_date_select_javascripts(options = {})
-    options.assert_valid_keys(:format, :locale)
+    options.assert_valid_keys(:locale)
     files = ["calendar_date_select/calendar_date_select"]
     files << "calendar_date_select/locale/#{options[:locale]}" if options[:locale]
     files << "calendar_date_select/#{CalendarDateSelect.format[:javascript_include]}" if CalendarDateSelect.format[:javascript_include]
@@ -20,10 +20,10 @@ module CalendarDateSelect::IncludesHelper
     @cds_already_included=true
     
     options = (Hash === args.last) ? args.pop : {}
-    options.assert_valid_keys(:style, :format, :locale)
+    options.assert_valid_keys(:style, :locale)
     options[:style] ||= args.shift
     
-    javascript_include_tag(*calendar_date_select_javascripts(:locale => options[:locale], :format => options[:format])) + "\n" +
+    javascript_include_tag(*calendar_date_select_javascripts(:locale => options[:locale])) + "\n" +
     stylesheet_link_tag(*calendar_date_select_stylesheets(:style => options[:style])) + "\n"
   end
 end
