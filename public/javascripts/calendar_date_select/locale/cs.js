cd030e2532e9571ba37297985ffb1bd15b3a758e
Tomas Barton <barton.tomas@gmail.com>
2010-12-13 13:28:16 +0100

    added Czech translation

---
+++ public/javascripts/calendar_date_select/locale/cs.js
@@ -0,0 +1,11 @@
+Date.weekdays = $w("Po Út St Čt Pá So Ne");
+Date.months = $w("Leden Únor Březen Duben Květen Červen Červenec Srpen Září Říjen Listopad Prosinec" );
+
+Date.first_day_of_week = 1
+
+_translations = {
+    "OK": "OK",
+    "Now": "Teď",
+    "Today": "Dnes",
+    "Clear": "Smazat"
+}
