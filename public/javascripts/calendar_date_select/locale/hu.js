6a1fe68b07c23eb2b31ab5537c6dc8c7c0ef4f84
KARASZI István <github@spam.raszi.hu>
2011-06-15 14:51:14 +0200

    hungarian translation added

---
+++ public/javascripts/calendar_date_select/locale/hu.js
@@ -0,0 +1,11 @@
+Date.weekdays = $w('h. k. sze. cs. p. szo. v.');
+Date.months = $w('január február március április május június július augusztus szeptember október november december');
+
+Date.first_day_of_week = 1;
+
+_translations = {
+  "OK": "OK",
+  "Now": "Most",
+  "Today": "Ma",
+  "Clear": "Bezárás"
+}
